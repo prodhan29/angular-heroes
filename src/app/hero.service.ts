4ba28e65ada88b05f6e20a838ffa880c18e4a231
nahid hasan <02nahid02@gmail.com>
2018-08-27 15:04:06 +0600

    completed chapter 7 'routing'

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -28,5 +28,11 @@ export class HeroService {
     return of(this.heroes);
   }
 
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(this.heroes.find(hero => hero.id === id));
+  }
+
   constructor(private messageService: MessageService) { }
 }
